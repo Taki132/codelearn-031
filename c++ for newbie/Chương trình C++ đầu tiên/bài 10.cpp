0a4b0c20a161a6b571828659bab305743bc2e4c9
Taki132 <nguyenthaison609@outlook.com>
2023-05-19 16:16:15 +0700

    new file:   "c++ for newbie/Ch\306\260\306\241ng tr\303\254nh C++ \304\221\341\272\247u ti\303\252n/a.out" 	new file:   "c++ for newbie/Ch\306\260\306\241ng tr\303\254nh C++ \304\221\341\272\247u ti\303\252n/b\303\240i 10.cpp" 	new file:   "c++ for newbie/Ch\306\260\306\241ng tr\303\254nh C++ \304\221\341\272\247u ti\303\252n/b\303\240i 9.cpp"

---
+++ c++ for newbie/Chương trình C++ đầu tiên/bài 10.cpp
@@ -0,0 +1,8 @@
+#include "iostream"
+int main(){
+  std::cout << "2468 + 1234 = "<<2468 + 1234<< std::endl;
+  std::cout << "2468 - 1234 = "<<2468 - 1234<< std::endl;
+  std::cout << "2468 * 1234 = "<<2468 * 1234 << std::endl;
+  std::cout << "2468 / 1234 = "<<2468 / 1234 ;
+  return 0;
+}

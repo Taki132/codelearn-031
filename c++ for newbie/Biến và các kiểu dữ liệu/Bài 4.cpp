365d97c012fab2a9a773117ae9e22dc4e1003e27
Taki132 <nguyenthaison609@outlook.com>
2023-05-20 15:16:22 +0700

    new file:   "c++ for newbie/Bi\341\272\277n v\303\240 c\303\241c ki\341\273\203u d\341\273\257 li\341\273\207u/B\303\240i 4" 	new file:   "c++ for newbie/Bi\341\272\277n v\303\240 c\303\241c ki\341\273\203u d\341\273\257 li\341\273\207u/B\303\240i 4.cpp" 	new file:   "c++ for newbie/Bi\341\272\277n v\303\240 c\303\241c ki\341\273\203u d\341\273\257 li\341\273\207u/B\303\240i 5" 	new file:   "c++ for newbie/Bi\341\272\277n v\303\240 c\303\241c ki\341\273\203u d\341\273\257 li\341\273\207u/B\303\240i 5.cpp" 	new file:   "c++ for newbie/Bi\341\272\277n v\303\240 c\303\241c ki\341\273\203u d\341\273\257 li\341\273\207u/B\303\240i 6.cpp"

---
+++ c++ for newbie/Biến và các kiểu dữ liệu/Bài 4.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main() {
+    string x = "Codelearn";
+    cout<<"Hello "<<x;
+    return 0;
+ }
---
+++ c++ for newbie/Biến và các kiểu dữ liệu/Bài 5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    //tên: Codelearn
+    string name = "Codelearn";
+    //năm sinh: 2019
+    string dob = "2019";
+    cout << "Name: " << name << endl;
+    cout << "Date of birth: " << dob;
+    return 0;
+}

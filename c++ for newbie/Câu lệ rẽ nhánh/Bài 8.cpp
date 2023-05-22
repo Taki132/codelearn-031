cb53135beb3f6bcf56050b96170e02c1672b4d7c
Taki132 <nguyenthaison609@outlook.com>
2023-05-22 16:30:19 +0700

    new file:   "c++ for newbie/C\303\242u l\341\273\207 r\341\272\275 nh\303\241nh/B\303\240i 8" 	modified:   "c++ for newbie/C\303\242u l\341\273\207 r\341\272\275 nh\303\241nh/B\303\240i 8.cpp" 	new file:   "c++ for newbie/C\303\242u l\341\273\207 r\341\272\275 nh\303\241nh/B\303\240i 9" 	new file:   "c++ for newbie/C\303\242u l\341\273\207 r\341\272\275 nh\303\241nh/B\303\240i 9.cpp"

---
+++ c++ for newbie/Câu lệ rẽ nhánh/Bài 8.cpp
@@ -0,0 +1,10 @@
+#include "iostream"
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    if (a<0 || a>10){
+    cout<<"The score is not valid";}
+    else {
+    cout<<"The score is valid";}
+}

21ecf6766d2a83bc63a0ad0c3084b24e55ae21c1
Taki132 <nguyenthaison609@outlook.com>
2023-05-23 21:05:04 +0700

    modified:   "c++ for newbie/V\303\262ng l\341\272\267p: for/B\303\240i 8" 	modified:   "c++ for newbie/V\303\262ng l\341\272\267p: for/B\303\240i 8.cpp"

--- c++ for newbie/Vòng lặp: for/Bài 8.cpp
+++ c++ for newbie/Vòng lặp: for/Bài 8.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int a;
     cin>>a;
-    for (int i = 1; i<a;i++){
+    for (int i = 1; i<=a;i++){
         if (a%i == 0){
             cout<<i<<" ";
         }

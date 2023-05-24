998b72200a693ee31ecf772f3dfd366d90fc6194
Taki132 <nguyenthaison609@outlook.com>
2023-05-24 21:11:17 +0700

    update 'B\303\240i 1.cpp"'

--- c++ for newbie/Mảng/Bài 1.cpp
+++ c++ for newbie/Mảng/Bài 1.cpp
@@ -1,5 +1,14 @@
 #include "iostream"
 using namespace std;
 int main(){
-    
+    int a[10];
+    int k;
+    for (int i=0; i<10; i++){
+        cin>>a[i];
+    }
+    for (int i=0; i<10; i++){
+        k+=a[i];
+    }
+    cout<<k;
+    return 0;
 }

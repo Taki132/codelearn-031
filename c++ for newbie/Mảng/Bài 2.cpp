6a8994a4e01c0a017dc1146a0fb4fdbabb7b3e98
Taki132 <nguyenthaison609@outlook.com>
2023-05-24 21:37:02 +0700

    update 'B\303\240i 1"' and 'B\303\240i 1.cpp"'

--- c++ for newbie/Mảng/Bài 2.cpp
+++ c++ for newbie/Mảng/Bài 2.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 int main(){
     int a[10];
-    for (int i=0; i<10; i++){
+    int 
+    for (int i=0; i<; i++){
         cin>>a[i];
     }
     int k = a[0];
     for (int i=0; i<10; i++){
         if (k<a[i]){
-            k+=a[i];
+            k=a[i];
         }
     }
     cout<<k;

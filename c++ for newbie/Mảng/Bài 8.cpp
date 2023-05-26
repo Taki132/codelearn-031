3c68893b7639867846935a011474d0e84a6cab08
Taki132 <nguyenthaison609@outlook.com>
2023-05-26 22:18:15 +0700

    add file

--- c++ for newbie/Mảng/Bài 8.cpp
+++ c++ for newbie/Mảng/Bài 8.cpp
@@ -3,16 +3,16 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    int a[n];
+    int a[1000];
     for (int i=0;i<n;i++){
         cin>>a[i];
     }
     for (int i=0;i<n;i++){
-        for (int k=i+1; j<n;j++){
-            if (a[i+1]<a[i]){
-                int k
-                a[i]=a[k]
-
+        for (int k=i+1; k<n;k++){
+            if (a[i]>a[k]){
+                int m = a[i];
+                a[i]=a[k];
+                a[k]=m;
             }
         }
     }
---
+++ c++ for newbie/Mảng/Bài 9.cpp
@@ -0,0 +1,18 @@
+#include "iostream"
+using namespace std;
+int main(){
+    int a,b;
+    int m[100][100];
+    for (int i=0;i<a;i++){
+        for (int o=0;o<b;o++){
+            cin>>m[a][b];
+        }
+    }
+    int sum;
+    for (int i=0;i<a;i++){
+        for (int o=0;o<b;o++){
+            sum+=a[i][o];
+        }
+    }
+    cout<<sum;
+}
